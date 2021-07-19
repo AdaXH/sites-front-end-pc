f01526b425be24a0340220dae9eb05def39570e7
ada <3532371088@qq.com>
2021-07-19 14:18:01 +0800

    styles: my-sites

--- src/pages/userCenter/components/myFavorite/index.tsx
+++ src/pages/userCenter/components/myFavorite/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { Table, Popconfirm } from 'antd';
+import { Popconfirm } from 'antd';
 import { useDidMount } from '@/utils/hooks';
-import { MAP_SITE_TYPE } from '@/utils/constant';
+// import { MAP_SITE_TYPE } from '@/utils/constant';
 import { queryMyFavorite, togglCollectSite } from './service';
+import { SiteItem } from '../siteItem';
 
 import styles from '../mySites/styles.less';
 
-export default () => {
+export default ({ history }) => {
   const [data, setData] = useState([]);
   useDidMount(async () => {
     const { data: resData } = await queryMyFavorite();
@@ -21,56 +22,21 @@ export default () => {
       setData(data.filter((item) => item._id !== siteId));
     }
   };
-  const columns = [
-    {
-      title: '名称',
-      render: ({ siteType, siteName, _id }) => (
-        <a
-          className={`${styles.item} ${styles.name}`}
-          onClick={() => window.open(`/site-info?siteId=${_id}&siteType=${siteType}`)}
-        >
-          {siteName}
-        </a>
-      ),
-    },
-    {
-      title: '分类',
-      render: ({ siteType }) => MAP_SITE_TYPE[siteType],
-    },
-    {
-      title: '点赞',
-      render: ({ siteUpvotes }) => (
-        <span className={styles.upvote}>
-          <i className="iconfont icondianzan" />
-          <span className={styles.upvoteNum}>{siteUpvotes.length}</span>
-        </span>
-      ),
-    },
-    {
-      title: '链接',
-      render: ({ siteLink }) => (
-        <a href={siteLink} className={styles.link} target="_blank" rel="noreferrer">
-          {siteLink}
-          <i className="iconfont iconlink" />
-        </a>
-      ),
-    },
-    {
-      title: '操作',
-      render: ({ siteType, siteName, _id, userId }) => (
-        <div className={styles.item}>
-          <Popconfirm
-            title={`确认取消收藏站点：${siteName}?`}
-            onConfirm={() => cancelFavorite(_id, siteType)}
-          >
-            <a className={styles.link}>取消收藏</a>
-          </Popconfirm>
-        </div>
-      ),
-    },
-  ];
 
   return (
-    <Table className={styles.table} rowKey={(d) => d._id} dataSource={data} columns={columns} />
+    <div className={styles.table}>
+      {data.map((item) => (
+        <SiteItem
+          key={item._id}
+          data={item}
+          renderOperation={({ _id: siteId, siteType }) => (
+            <Popconfirm title="确认取消收藏？" onConfirm={() => cancelFavorite(siteId, siteType)}>
+              <a>取消收藏</a>
+            </Popconfirm>
+          )}
+          history={history}
+        />
+      ))}
+    </div>
   );
 };
--- src/pages/userCenter/components/mySites/index.tsx
+++ src/pages/userCenter/components/mySites/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Popconfirm, Table } from 'antd';
+import { Popconfirm } from 'antd';
 import { useDidMount } from '@/utils/hooks';
-import { MAP_SITE_TYPE } from '@/utils/constant';
+import { SiteItem } from '../siteItem';
 import { queryMySites, deleteSite } from './service';
 
 import styles from './styles.less';
 
-export default ({ reLoad }) => {
+export default ({ reLoad, history }) => {
   const [data, setData] = useState([]);
   useDidMount(async () => {
     const { data: resData } = await queryMySites();
@@ -22,61 +22,20 @@ export default ({ reLoad }) => {
       reLoad();
     }
   };
-  const columns = [
-    {
-      title: '名称',
-      render: ({ siteType, siteName, _id }) => (
-        <a
-          className={`${styles.item} ${styles.name}`}
-          onClick={() => window.open(`/site-info?siteId=${_id}&siteType=${siteType}`)}
-        >
-          {siteName}
-        </a>
-      ),
-    },
-    {
-      title: '分类',
-      render: ({ siteType }) => MAP_SITE_TYPE[siteType],
-    },
-    {
-      title: '点赞',
-      render: ({ siteUpvotes }) => (
-        <span className={styles.upvote}>
-          <i className="iconfont icondianzan" />
-          <span className={styles.upvoteNum}>{siteUpvotes.length}</span>
-        </span>
-      ),
-    },
-    {
-      title: '链接',
-      render: ({ siteLink }) => (
-        <a href={siteLink} className={styles.link} target="_blank" rel="noreferrer">
-          {siteLink}
-          <i className="iconfont iconlink" />
-        </a>
-      ),
-    },
-    {
-      title: '操作',
-      render: ({ siteType, siteName, _id, userId }) => (
-        <div className={styles.item}>
-          <a
-            className={styles.link}
-            onClick={() => window.open(`/submit-site?siteId=${_id}&siteType=${siteType}`)}
-          >
-            编辑
-          </a>
-          <Popconfirm
-            title={`确认删除站点：${siteName}?`}
-            onConfirm={() => onDelete(_id, userId, siteType)}
-          >
-            <a className={styles.link}>删除</a>
-          </Popconfirm>
-        </div>
-      ),
-    },
-  ];
   return (
-    <Table rowKey={(d) => d._id} className={styles.table} dataSource={data} columns={columns} />
+    <div className={styles.table}>
+      {data.map((item) => (
+        <SiteItem
+          key={item._id}
+          data={item}
+          renderOperation={({ _id: siteId, siteType, userId }) => (
+            <Popconfirm title="确认删除？" onConfirm={() => onDelete(siteId, userId, siteType)}>
+              <a>删除</a>
+            </Popconfirm>
+          )}
+          history={history}
+        />
+      ))}
+    </div>
   );
 };
