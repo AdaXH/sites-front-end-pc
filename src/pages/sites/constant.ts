8e2aa392627a7751488761462171192bb53934cb
Ada <adaxh@qq.com>
2021-09-12 19:58:04 +0800

    temp-save

--- src/pages/sites/constant.ts
+++ src/pages/sites/constant.ts
@@ -3,11 +3,13 @@ export const SITES_ROUTES = [
     path: '/sites/technology',
     component: () => import('@/pages/technology'),
     title: '技术',
+    mainTitle: 'Technology',
     shouMenu: true,
   },
   {
     path: '/sites/life',
     component: () => import('@/pages/life/index'),
+    mainTitle: 'Life',
     shouMenu: true,
     title: '生活',
   },
@@ -15,12 +17,14 @@ export const SITES_ROUTES = [
     path: '/sites/info',
     component: () => import('@/pages/info/index'),
     shouMenu: true,
+    mainTitle: 'Info',
     title: '资讯',
   },
   {
     path: '/sites/others',
     component: () => import('@/pages/others/index'),
     shouMenu: true,
+    mainTitle: 'Others',
     title: '其它',
   },
   {
--- src/pages/sites/index.tsx
+++ src/pages/sites/index.tsx
@@ -1,20 +1,40 @@
+import { useMemo } from 'react';
 import { Switch, Router, Redirect } from 'dva/router';
 import Layout from '@/layout';
 
 import { mapRouteMethod } from '@/router/util';
 import React from 'react';
 import { SITES_ROUTES } from './constant';
+import styles from './styles.less';
+import { PageSlide } from '@/component/pageSlide';
 
-export default ({ history }) => {
+const menus = SITES_ROUTES.map(({ title, path, mainTitle }) => ({ title, mainTitle, path }));
+
+export default ({ history }: { history: History }) => {
+  const {
+    location: { pathname },
+  } = history;
+  const curRoute = useMemo(() => SITES_ROUTES.find((item) => item.path === pathname), [pathname]);
   return (
-    <div>
-      sites
+    <div className={styles.sites}>
+      <div className={styles.pageBox}>
+        <PageSlide current={1} total={menus.length} />
+      </div>
       {/* <Router history={history}> */}
-      <Switch>
-        {mapRouteMethod(SITES_ROUTES)}
-        {/* <Redirect to="/" /> */}
-      </Switch>
-      {/* </Router> */}
+      <header className={styles.menus}>
+        {menus.map(({ title, path, mainTitle }) => (
+          <div key={title} className={styles.menusItem} onClick={() => history.push(path)}>
+            {title}
+          </div>
+        ))}
+      </header>
+      <section className={styles.sitesCon}>
+        <h3 className={styles.sitesConTitle}>{curRoute?.mainTitle}</h3>
+        <Switch>
+          {mapRouteMethod(SITES_ROUTES)}
+          <Redirect to="/sites/technology" />
+        </Switch>
+      </section>
     </div>
   );
 };
