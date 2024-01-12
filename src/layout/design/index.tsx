1aa958bd3802e7e52f979efcc58abe3e3e834538
Ada <Adaxh@qq.com>
2024-01-12 14:55:07 +0800

    fix: icp

--- src/layout/design/index.tsx
+++ src/layout/design/index.tsx
@@ -10,7 +10,8 @@ export default memo(() => {
         data-line
         href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602011734"
       >
-        NO : 33010602011734
+        {/* NO : 33010602011734 */}
+        <p>浙公网安备33010602011734号 滇ICP备2020009525号-3</p>
       </a>
       {/* . */}
       {/* <a data-line href="/global-feed.xml" target="_blank">
