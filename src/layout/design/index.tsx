46181eb637acf9127d7da03835e87bbea4832c83
xiangyl51186 <xiangyl51186@hundsun.com>
2024-01-19 16:59:02 +0800

    refactor:icp

--- src/layout/design/index.tsx
+++ src/layout/design/index.tsx
@@ -5,14 +5,19 @@ export default memo(() => {
   return (
     <footer className={styles.footer}>
       Copyright (c) Sites Group All Rights Reserved.{' '}
-      <a
-        target="blank"
-        data-line
-        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602011734"
-      >
-        {/* NO : 33010602011734 */}
-        <p>浙公网安备33010602011734号 滇ICP备2020009525号-3</p>
-      </a>
+      <div>
+        <a
+          target="blank"
+          data-line
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602011734"
+        >
+          {/* NO : 33010602011734 */}
+          浙公网安备33010602011734号
+        </a>
+        <a data-line href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">
+          滇ICP备2020009525号-3
+        </a>
+      </div>
       {/* . */}
       {/* <a data-line href="/global-feed.xml" target="_blank">
         <i className="iconrss1 iconfont" />
